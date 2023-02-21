2902ffea15acc56d73b942c2cb26967b2b8d70fa
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-02-21 10:52:37 +0100

    Add files via upload

--- src/ch/epfl/javions/Units.java
+++ src/ch/epfl/javions/Units.java
@@ -2,23 +2,19 @@ package ch.epfl.javions;
 
 public final class Units {
 
-    public static final double CENTI = 1e-2;
-    public static final double KILO = 1e3;
+    public static final double CENTI = 0.01;
+    public static final double KILO = 1000;
 
     private Units() {
 
     }
 
-   public static double convert(double value, double fromUnit, double toUnit){
-        return value*(fromUnit/toUnit);
-   }
-
     public static class Angle {
 
-        public static final double RADIAN = 1;
-        public static final double TURN = 2 * Math.PI * RADIAN;
-        public static final double DEGREE = TURN/360;
-        public static final double T32 = TURN/Math.scalb(1, 32);
+        private static final double RADIAN = 1;
+        private static final double TURN = 2 * Math.PI * RADIAN;
+        private static final double DEGREE = TURN/360;
+        private static final double T32 = TURN/Math.pow(2, 32);
 
         private Angle() {
 
@@ -28,12 +24,8 @@ public final class Units {
 
     public static class Length {
 
-        public static final double METER = 1;
-        public static final double CENTIMETER = CENTI*METER;
-        public static final double KILOMETER = KILO*METER;
-        public static final double INCH = CENTIMETER*2.54;
-        public static final double FOOT = 12*INCH;
-        public static final double NAUTICAL_MILE = 1852*METER;
+        private static final double METER = 1;
+        
 
         private Length() {
         }
@@ -41,9 +33,9 @@ public final class Units {
 
     public static class Time {
 
-        public static final double SECOND = 1;
-        public static final double MINUTE = 60*SECOND;
-        public static final double HOUR = 60*MINUTE;
+        private static final double SECOND = 1;
+        private static final double MINUTE = 60*SECOND;
+        private static final double HOUR = 60*MINUTE;
 
         private Time() {
         }
@@ -51,12 +43,9 @@ public final class Units {
 
     public static class Speed {
 
-        public static final double METER_PER_SECOND = Length.METER/Time.SECOND;
-        public static final double KNOT = Length.NAUTICAL_MILE/Time.HOUR;
-        public static final double KILOMETER_PER_HOUR  = Length.KILOMETER/Time.HOUR;
-        
         private Speed() {
         }
     }
 
 }
+
