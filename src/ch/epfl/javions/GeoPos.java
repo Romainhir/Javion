2523435540673c94a72b14a7cd6fb600cd914d63
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-02-24 00:42:40 +0100

    GeoPos started

--- src/ch/epfl/javions/GeoPos.java
+++ src/ch/epfl/javions/GeoPos.java
@@ -2,19 +2,44 @@ package ch.epfl.javions;
 
 public record GeoPos(int longitudeT32, int latitudeT32) {
 
+    /**
+     * Constructor
+     * @param longitudeT32 longitude
+     * @param latitudeT32 latitude
+     */
     public GeoPos{
         if (!(isValidLatitudeT32(latitudeT32))){ throw new IllegalArgumentException();}
     }
+
+    /**
+     * Check si la latitude est valide
+     * @param latitudeT32 latitude
+     * @return boolean
+     */
     public static boolean isValidLatitudeT32(int latitudeT32){
         return -Math.scalb(1, 30) <= latitudeT32 & latitudeT32 <= Math.scalb(1, 30);
     }
 
+    /**
+     * Retourne la longitude en radian
+     * @return longitude
+     */
     public double longitude(){return Units.convert(longitudeT32, Units.Angle.T32, Units.Angle.RADIAN);}
+
+    /**
+     * Retourne la latitude en radian
+     * @return latitude
+     */
     public double latitude(){return Units.convert(latitudeT32, Units.Angle.T32, Units.Angle.RADIAN);}
+
+    /**
+     * Redefinit toString pour renvoyer la longitude et latitude en degrés
+     * @return String
+     */
     @Override
     public String toString() {
         return "("+(Units.convertTo(longitude(), Units.Angle.DEGREE)+
-                ","+Units.convertTo(latitude(), Units.Angle.DEGREE))+")";
+                "°,"+Units.convertTo(latitude(), Units.Angle.DEGREE))+"°)";
     }
 
 }
