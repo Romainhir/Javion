65738b4529d5c67e104594e2651684d1d9812d48
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-02-24 14:08:50 +0100

    GeoPos started

--- src/ch/epfl/javions/WebMercator.java
+++ src/ch/epfl/javions/WebMercator.java
@@ -4,12 +4,12 @@ public class WebMercator {
 
     private WebMercator(){}
 
-    public double x(int zoomLevel, double longitude){
+    public static double x(int zoomLevel, double longitude){
         return Math.scalb(1, 8+zoomLevel) *
                 (Units.convertTo(longitude, Units.Angle.TURN) + 0.5);
     }
 
-    public double y(int zoomLevel, double latitude){
+    public static double y(int zoomLevel, double latitude){
         return Math.scalb(1, 8+zoomLevel) * (-Units.convertTo
                 (Math2.asinh(Math.tan(latitude)), Units.Angle.TURN) + 0.5);
 
