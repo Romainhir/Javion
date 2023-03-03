c07e348870e526d9c42e92346f43d3d49d23526a
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-03-03 13:12:01 +0100

    ?

--- test/ch/epfl/javions/p2/AircraftDescriptionTest.java
+++ test/ch/epfl/javions/p2/AircraftDescriptionTest.java
@@ -1,7 +1,6 @@
 package ch.epfl.javions.p2;
 
 import ch.epfl.javions.aircraft.AircraftDescription;
-<<<<<<< HEAD
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -10,68 +9,69 @@ public class AircraftDescriptionTest {
 
     @Test
     void aircraftDescriptionConstructionSuccess() {
-        assertDoesNotThrow(() -> {new AircraftDescription("A0E");});
-        assertDoesNotThrow(() -> {new AircraftDescription("B1J");});
-        assertDoesNotThrow(() -> {new AircraftDescription("D2P");});
-        assertDoesNotThrow(() -> {new AircraftDescription("G3T");});
-        assertDoesNotThrow(() -> {new AircraftDescription("H4-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("L6-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("P8-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("R8-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("S8-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("T4-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("V8-");});
-        assertDoesNotThrow(() -> {new AircraftDescription("-1-");});
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("A0E");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("B1J");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("D2P");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("G3T");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("H4-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("L6-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("P8-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("R8-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("S8-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("T4-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("V8-");
+        });
+        assertDoesNotThrow(() -> {
+            new AircraftDescription("-1-");
+        });
     }
 
     @Test
     void aircraftDescriptionConstructionFailure() {
-        assertThrows(NullPointerException.class, () -> {new AircraftDescription(null);});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("A");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("A1");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("9-");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("EA1");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("BITE");});
-        assertThrows(IllegalArgumentException.class, () -> {new AircraftDescription("é$à");});
-=======
-import ch.epfl.javions.aircraft.AircraftRegistration;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class AircraftDescriptionTest {
-    @Test
-    void AircraftDescriptionConstructionSuccess() {
-        assertDoesNotThrow(() -> {
-            new AircraftDescription("V6P");
-        });
-        assertDoesNotThrow(() -> {
-            new AircraftDescription("-0-");
-        });
-        assertDoesNotThrow(() -> {
-            new AircraftDescription("V8J");
-        });
-    }
-
-    @Test
-    void AircraftDescriptionConstructionFailure() {
-        /*assertThrows(IllegalArgumentException.class, () -> {
+        assertThrows(NullPointerException.class, () -> {
             new AircraftDescription(null);
-        });*/
-        assertThrows(IllegalArgumentException.class, () -> {
-            new AircraftDescription("%@*°^^");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            new AircraftDescription("çàléèöüä");
-        });
-        assertThrows(IllegalArgumentException.class, () -> {
-            new AircraftDescription("objection");
         });
         assertThrows(IllegalArgumentException.class, () -> {
             new AircraftDescription("");
         });
->>>>>>> origin/main
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("A");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("A1");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("9-");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("EA1");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("BITE");
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new AircraftDescription("é$à");
+        });
     }
 }
