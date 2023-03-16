86af78a91ad5b46e918549c6ffa9079701b3fc71
Romainhir <romain.hirschi@epfl.ch>
2023-03-16 14:14:06 +0100

    OK

--- src/ch/epfl/javions/demodulation/AdsbDemodulator.java
+++ src/ch/epfl/javions/demodulation/AdsbDemodulator.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package ch.epfl.javions.demodulation;
 
 import ch.epfl.javions.ByteString;
@@ -69,3 +70,47 @@ public final class AdsbDemodulator {
         return null;
     }
 }
+=======
+package ch.epfl.javions.demodulation;
+
+import ch.epfl.javions.adsb.RawMessage;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+
+public final class AdsbDemodulator {
+
+    private final InputStream samplesStream;
+    private final PowerWindow powerWindow;
+
+    public final static int WINDOWSIZE = 1200;
+    public AdsbDemodulator(InputStream samplesStream) throws IOException {
+        this.samplesStream = samplesStream;
+        powerWindow = new PowerWindow(samplesStream, WINDOWSIZE);
+
+    }
+
+    private boolean preambleFound() {
+       int peakSample = powerWindow.get(0) + powerWindow.get(10) +powerWindow.get(35)
+               + powerWindow.get(45);
+
+       int valleySample = powerWindow.get(5) + powerWindow.get(15) + powerWindow.get(20)
+               + powerWindow.get(25) + powerWindow.get(30) + powerWindow.get(40);
+
+       return (peakSample > 2 * valleySample);
+    }
+    public RawMessage nextMessage() throws IOException{
+        while (powerWindow.isFull()) {
+            if (preambleFound()) {
+                return null;
+            }else {
+                powerWindow.advance();
+                    }
+
+        }
+        return null;
+    }
+}
+>>>>>>> Stashed changes
--- src/ch/epfl/javions/demodulation/PowerWindow.java
+++ src/ch/epfl/javions/demodulation/PowerWindow.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package ch.epfl.javions.demodulation;
 
 import ch.epfl.javions.Preconditions;
@@ -77,4 +78,85 @@ public final class PowerWindow {
         }
     }
 
+=======
+package ch.epfl.javions.demodulation;
+
+import ch.epfl.javions.Preconditions;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+
+public final class PowerWindow {
+
+    private final int TAB_LENGTH = (1 << 16);
+
+    private int batchSize;
+    private int batchSize2;
+    private int windowsSize;
+    private long index;
+
+    private int[] windowHelper;
+    private int[] windowMain;
+    private PowerComputer computer;
+
+    public PowerWindow(InputStream stream, int windowSize) throws IOException {
+        Preconditions.checkArgument((windowSize > 0) && (windowSize <= TAB_LENGTH));
+        computer = new PowerComputer(stream, TAB_LENGTH);
+        windowMain = new int[TAB_LENGTH];
+        windowHelper = new int[TAB_LENGTH];
+        batchSize = computer.readBatch(windowMain);
+        this.windowsSize = windowSize;
+        batchSize2 = 0;
+    }
+
+    public int size() {
+        return windowsSize;
+    }
+
+    public long position() {
+        return index;
+    }
+
+    public boolean isFull() {
+        if (batchSize + batchSize2 < index % TAB_LENGTH + windowsSize) {
+            return false;
+        }
+        return true;
+    }
+
+    public int get(int i) throws IndexOutOfBoundsException {
+        Objects.checkIndex(i, windowsSize);
+        if (((int) ((index + i) / (TAB_LENGTH)) == (index / TAB_LENGTH))) {
+            return windowMain[(int) ((index % TAB_LENGTH) + i)];
+        } else {
+            return windowHelper[(int) ((index + i) % TAB_LENGTH)];
+        }
+    }
+
+    private void switchTab() {
+        int[] tab = windowMain;
+        windowMain = windowHelper;
+        windowHelper = tab;
+        batchSize = batchSize2;
+        batchSize2 = 0;
+    }
+
+    public void advance() throws IOException {
+        index++;
+        if (index % TAB_LENGTH + windowsSize - 1 == TAB_LENGTH) {
+            batchSize2 = computer.readBatch(windowHelper);
+        } else if (index % TAB_LENGTH == 0) {
+            switchTab();
+        }
+    }
+
+    public void advanceBy(int offset) throws IOException {
+        Preconditions.checkArgument(offset >= 0);
+        for (int i = 0; i < offset; i++) {
+            advance();
+        }
+    }
+
+>>>>>>> Stashed changes
 }
