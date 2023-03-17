b93732d50f2f2072edffd353041feb5dd6d6c9e5
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-03-17 14:45:26 +0100

    o

--- src/ch/epfl/cs108/Submit.java
+++ src/ch/epfl/cs108/Submit.java
@@ -32,9 +32,9 @@ public final class Submit {
     // CONFIGURATION
     // -------------
     // Jeton du premier membre du groupe
-    private static final String TOKEN_1 = "ud1Zog3b";
+    private static final String TOKEN_1 = "gee1ieTu";
     // Jeton du second membre (identique au premier pour les personnes travaillant seules)
-    private static final String TOKEN_2 = "Ri1oonoo";
+    private static final String TOKEN_2 = "Phu8rah8";
     // -------------
 
     private static final String ZIP_ENTRY_NAME_PREFIX = "Javions/";
