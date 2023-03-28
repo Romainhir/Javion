c1642e062b7a6c10a46bda62b327fdbb6a3b9c40
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-03-29 00:47:04 +0200

    yes

--- src/ch/epfl/javions/adsb/MessageParser.java
+++ src/ch/epfl/javions/adsb/MessageParser.java
@@ -7,16 +7,12 @@ public class MessageParser {
 
     public static Message parse(RawMessage rawMessage) {
         //TODO Moche xd
-        switch (rawMessage.typeCode()) {
-            case 1, 2, 3, 4:
-                return AircraftIdentificationMessage.of(rawMessage);
-            case 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22:
-                return AirbornePositionMessage.of(rawMessage);
-            case 19:
-                return AirborneVelocityMessage.of(rawMessage);
-            default:
-                return null;
-        }
+        return switch (rawMessage.typeCode()) {
+            case 1, 2, 3, 4 -> AircraftIdentificationMessage.of(rawMessage);
+            case 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 21, 22 -> AirbornePositionMessage.of(rawMessage);
+            case 19 -> AirborneVelocityMessage.of(rawMessage);
+            default -> null;
+        };
     }
 
 
