c6a658d74b144c9c9dd257f304585eaba5e05cac
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-04-23 05:42:54 +0200

    yes

---
+++ src/ch/epfl/javions/gui/ObservableAircraftState.java
@@ -0,0 +1,146 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.adsb.AircraftStateSetter;
+import ch.epfl.javions.adsb.CallSign;
+import ch.epfl.javions.aircraft.IcaoAddress;
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class ObservableAircraftState implements AircraftStateSetter {
+    private LongProperty lastMessageTimeStampNs;
+    private IntegerProperty category;
+    private ObjectProperty<CallSign> callSign;
+    private ObjectProperty<GeoPos> position;
+    private final ObservableList<Object> airbornePos = FXCollections.observableArrayList();
+    private ObservableList<Object> airbornePosSecond = FXCollections.unmodifiableObservableList(airbornePos);
+
+
+    private DoubleProperty altitude;
+    private DoubleProperty velocity;
+    private DoubleProperty trackOrHeading;
+
+
+    public ObservableAircraftState(IcaoAddress icaoAddress) {
+
+    }
+
+    public ReadOnlyLongProperty lastMessageTimeStampNsProperty() {
+        return lastMessageTimeStampNs;
+    }
+
+    public ReadOnlyIntegerProperty categoryProperty() {
+        return category;
+    }
+
+    public ReadOnlyObjectProperty<CallSign> callSignProperty() {
+        return callSign;
+    }
+
+    public ReadOnlyObjectProperty<GeoPos> positionProperty() {
+        return position;
+    }
+
+    public ObservableList<Object> airbornePosProperty() {
+        return airbornePosSecond;
+    }
+
+    public ReadOnlyDoubleProperty altitudeProperty() {
+        return altitude;
+    }
+
+    public ReadOnlyDoubleProperty velocityProperty() {
+        return velocity;
+    }
+
+    public ReadOnlyDoubleProperty trackOrHeadingProperty() {
+        return trackOrHeading;
+    }
+
+    public long getLastMessageTimeStampNs() {
+        return lastMessageTimeStampNs.get();
+    }
+
+    public int getCategory() {
+        return category.get();
+    }
+
+    public CallSign getCallSign() {
+        return callSign.get();
+    }
+
+    public GeoPos getPosition() {
+        return position.get();
+    }
+
+    public List<AirbornePos> getAirbornePos() {
+        List<AirbornePos> list = new ArrayList<>();
+        for (Object o : airbornePos) {
+            list.add((AirbornePos) o);
+        }
+        return list;
+    }
+
+    public double getAltitude() {
+        return altitude.get();
+    }
+
+    public double getVelocity() {
+        return velocity.get();
+    }
+
+    public double getTrackOrHeading() {
+        return trackOrHeading.get();
+    }
+
+    @Override
+    public void setLastMessageTimeStampNs(long timeStampNs) {
+        this.lastMessageTimeStampNs.set(timeStampNs);
+    }
+
+    @Override
+    public void setCategory(int category) {
+        this.category.set(category);
+    }
+
+    @Override
+    public void setCallSign(CallSign callSign) {
+        this.callSign.set(callSign);
+    }
+
+    @Override
+    public void setPosition(GeoPos position) {
+        if (!(this.position.get().equals(position))){
+            airbornePos.add(new AirbornePos(position, altitude.get()));
+            airbornePosSecond = FXCollections.unmodifiableObservableList(airbornePos);
+        }
+        this.position.set(position);
+
+    }
+
+    @Override
+    public void setAltitude(double altitude) {
+        if (this.altitude.get() != altitude){
+            airbornePos.add(new AirbornePos(position.get(), altitude));
+            airbornePosSecond = FXCollections.unmodifiableObservableList(airbornePos);
+        }
+        this.altitude.set(altitude);
+    }
+
+    @Override
+    public void setVelocity(double velocity) {
+        this.velocity.set(velocity);
+    }
+
+    @Override
+    public void setTrackOrHeading(double trackOrHeading) {
+        this.trackOrHeading.set(trackOrHeading);
+    }
+
+    public record AirbornePos(GeoPos pos, double altitude) {
+    }
+}
