62e4d8d904907feab3f13505da70e5fed45e3083
Vengiro <120104662+Vengiro@users.noreply.github.com>
2023-05-14 18:22:42 +0200

    u

--- src/ch/epfl/javions/gui/Main.java
+++ src/ch/epfl/javions/gui/Main.java
@@ -7,6 +7,8 @@ import ch.epfl.javions.adsb.RawMessage;
 import ch.epfl.javions.aircraft.AircraftDatabase;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.SplitPane;
@@ -20,10 +22,22 @@ import java.net.URL;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 public final class Main extends Application {
 
-    static List<RawMessage> readAllMessages(String fileName)
+    private final ConcurrentLinkedQueue<RawMessage> messageQueue = new ConcurrentLinkedQueue<>();
+
+    private static final int WIDTH_MIN = 800;
+    private static final int HEIGHT_MIN = 600;
+    private static final int X_COORD = 33_530;
+    private static final int Y_COORD = 23_070;
+    private static final int ZOOM = 8;
+
+
+
+    private List<RawMessage> readAllMessages(String fileName)
             throws IOException {
         List<RawMessage> list = new ArrayList<>();
         try (DataInputStream s = new DataInputStream(
@@ -44,15 +58,14 @@ public final class Main extends Application {
         return list;
     }
 
-    public static void main(String[] args) {
-        launch(args); }
+    public static void main(String[] args) {launch(args); }
     @Override
     public void start(Stage primaryStage) throws Exception {
 
         TileManager tm =
                 new TileManager(Path.of("tile-cache"), "tile.openstreetmap.org");
         MapParameters mp =
-                new MapParameters(8, 33_530, 23_070);
+                new MapParameters(ZOOM, X_COORD, Y_COORD);
         BaseMapController bmc = new BaseMapController(tm, mp);
 
         URL u = getClass().getResource("/aircraft.zip");
@@ -61,37 +74,63 @@ public final class Main extends Application {
         AircraftDatabase db = new AircraftDatabase(p.toString());
 
        AircraftStateManager asm = new AircraftStateManager(db);
-
-       AircraftController ac = new AircraftController(mp, asm.getStates(), new  SimpleObjectProperty<>());
-       AircraftTableController atc = new AircraftTableController(asm.getStates(), new  SimpleObjectProperty<>());
-       StatusLineController slc = new StatusLineController(asm);
+        ObjectProperty<ObservableAircraftState> observedAircraft = new SimpleObjectProperty<>();
+       AircraftController ac = new AircraftController(mp, asm.getStates(), observedAircraft);
+       AircraftTableController atc = new AircraftTableController(asm.getStates(), observedAircraft);
+       StatusLineController slc = new StatusLineController();
 
        SplitPane sp = new SplitPane();
        StackPane stkPane = new StackPane();
        BorderPane bPane = new BorderPane();
+
+       slc.aircraftCountProperty().bind(Bindings.size(asm.getStates()));
 
        stkPane.getChildren().add(bmc.pane());
        stkPane.getChildren().add(ac.pane());
        bPane.centerProperty().set(atc.pane());
        bPane.topProperty().set(slc.pane());
        sp.getItems().addAll(stkPane, bPane);
-       sp.setMinWidth(800);
-       sp.setMinHeight(600);
+       sp.setMinWidth(WIDTH_MIN);
+       sp.setMinHeight(HEIGHT_MIN);
        sp.setOrientation(javafx.geometry.Orientation.VERTICAL);
+       atc.setOnDoubleClick(selected -> bmc.centerOn(selected.getPosition()));
 
-        primaryStage.setScene(new Scene(sp));
-        primaryStage.show();
+       primaryStage.setScene(new Scene(sp));
+       primaryStage.show();
 
-        List<RawMessage> list = readAllMessages("resources/messages_20230318_0915.bin");
-        var mi = list.iterator();
+
+
+        new Thread(() -> {
+
+            try (DataInputStream s = new DataInputStream(
+                    new BufferedInputStream(
+                            new FileInputStream("resources/messages_20230318_0915.bin")))) {
+                byte[] bytes = new byte[RawMessage.LENGTH];
+                int i = 0;
+                long lastTimeStampNs = 0;
+                while (i < s.available()) {
+                    i++;
+                    long timeStampNs = s.readLong();
+                    int bytesRead = s.readNBytes(bytes, 0, bytes.length);
+                    assert bytesRead == RawMessage.LENGTH;
+                    ByteString message = new ByteString(bytes);
+                    messageQueue.add(new RawMessage(timeStampNs, message));
+                    long delta = timeStampNs - lastTimeStampNs;
+                    Thread.sleep(delta / 1_000_000);
+                    lastTimeStampNs = timeStampNs;
+                }
+        } catch (IOException | InterruptedException e2) {}
+        }).start();
 
         new AnimationTimer() {
             @Override
             public void handle(long now) {
                 try {
-                    for (int i = 0; i < 2; i += 1) {
-                        Message m = MessageParser.parse(mi.next());
-                        if (m != null) asm.update(m);
+                    if(!(messageQueue.isEmpty())) {
+                        RawMessage m = messageQueue.poll();
+                        Message message = MessageParser.parse(m);
+                        asm.update(message);
+                        slc.messageCountProperty().set(slc.messageCountProperty().get() + 1);
                     }
                 } catch (IOException e) {
                     throw new UncheckedIOException(e);
--- src/ch/epfl/javions/gui/StatusLineController.java
+++ src/ch/epfl/javions/gui/StatusLineController.java
@@ -5,6 +5,7 @@ import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.LongProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleLongProperty;
+import javafx.collections.SetChangeListener;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
@@ -14,22 +15,19 @@ public final class StatusLineController {
     private IntegerProperty numberOfAircrafts = new SimpleIntegerProperty(0);
     private LongProperty messageCount = new SimpleLongProperty(0);
     private BorderPane pane = new BorderPane();
-    private AircraftStateManager asm;
 
-    public StatusLineController(AircraftStateManager asm){
-        this.asm = asm;
-    }
 
 
     public Pane pane(){
         pane.getStylesheets().add("status.css");
 
         Text nbOfAircraft = new Text();
-        numberOfAircrafts.bind(Bindings.size(asm.getStates()));
-        nbOfAircraft.textProperty().bind(Bindings.format("Aéronefs visibles : %d", numberOfAircrafts.get()));
+        nbOfAircraft.textProperty().bind(Bindings.format("Aéronefs visibles : %d", numberOfAircrafts.get(),
+                numberOfAircrafts));
         pane.leftProperty().set(nbOfAircraft);
 
-        Text nbOfMessages = new Text("Messages reçus : " + messageCount.get());
+        Text nbOfMessages = new Text();
+        nbOfMessages.textProperty().bind(Bindings.format("Messages reçus : %d", messageCount.get()));
         pane.rightProperty().set(nbOfMessages);
         return pane;
 
