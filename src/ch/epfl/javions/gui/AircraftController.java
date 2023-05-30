60a90dd06baedd606300b17207bf5cb4d568d2b8
Romainhir <romain.hirschi@epfl.ch>
2023-05-30 12:16:47 +0200

    Cleaning

--- src/ch/epfl/javions/gui/AircraftController.java
+++ src/ch/epfl/javions/gui/AircraftController.java
@@ -3,30 +3,29 @@ package ch.epfl.javions.gui;
 import ch.epfl.javions.GeoPos;
 import ch.epfl.javions.Units;
 import ch.epfl.javions.WebMercator;
-import ch.epfl.javions.aircraft.*;
-import com.sun.javafx.collections.UnmodifiableObservableMap;
-import javafx.beans.InvalidationListener;
+import ch.epfl.javions.aircraft.AircraftData;
+import ch.epfl.javions.aircraft.AircraftDescription;
+import ch.epfl.javions.aircraft.AircraftTypeDesignator;
+import ch.epfl.javions.aircraft.WakeTurbulenceCategory;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.DoubleBinding;
 import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SetProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleSetProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableSet;
 import javafx.collections.SetChangeListener;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.text.Text;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Class used to control the view of the aircraft on the map. It stores and shows also the properties of the selected
@@ -83,8 +82,11 @@ public final class AircraftController {
     }
 
     private void addAircraft(ObservableAircraftState state) {
+        //Create the icon and the svg path
         AircraftIcon icon = createIcon(state);
         SVGPath path = new SVGPath();
+
+        //Set up the svg path
         path.setContent(icon.svgPath());
         path.setViewOrder(state.altitudeProperty().negate().get());
         path.viewOrderProperty().bind(state.altitudeProperty().negate());
@@ -92,28 +94,29 @@ public final class AircraftController {
         path.setLayoutY(setYPosition(state.getPosition()));
         path.setFill(ColorRamp.PLASMA.at(colorFormula(state.getAltitude())));
         path.setStroke(Color.BLACK);
-        state.trackOrHeadingProperty().addListener((observable, oldValue, newValue) -> {
-            path.setRotate(icon.canRotate() ? Units.convertTo(newValue.doubleValue(), Units.Angle.DEGREE) : 0);
-        });
-        path.setOnMouseClicked(event -> {
-            selectedAircraft.set(state);
-        });
+        path.setOnMouseClicked(event -> selectedAircraft.set(state));
+        path.setId(state.getIcaoAddress().string());
+
+        //Set up listener to update the position/orientation of the icon
+        state.trackOrHeadingProperty().addListener((observable, oldValue, newValue) ->
+                path.setRotate(icon.canRotate() ? Units.convertTo(newValue.doubleValue(), Units.Angle.DEGREE) : 0));
         state.positionProperty().addListener((observable, oldValue, newValue) -> {
             path.setLayoutX(setXPosition(newValue));
             path.setLayoutY(setYPosition(newValue));
         });
-        parameters.minXProperty().addListener((observable, oldValue, newValue) -> {
-            path.setLayoutX(setXPosition(state.getPosition()));
-        });
-        parameters.minYProperty().addListener((observable, oldValue, newValue) -> {
-            path.setLayoutY(setYPosition(state.getPosition()));
-        });
-        state.trackOrHeadingProperty().addListener((observable, oldValue, newValue) -> {
-            state.setPosition(state.getPosition());
-        });
         state.altitudeProperty().addListener((observable, oldValue, newValue) ->
                 path.setFill(ColorRamp.PLASMA.at(colorFormula(newValue.doubleValue()))));
+
+        //Set up listener to relocate icon if the map is moved
+        parameters.minXProperty().addListener((observable, oldValue, newValue) ->
+                path.setLayoutX(setXPosition(state.getPosition())));
+        parameters.minYProperty().addListener((observable, oldValue, newValue) ->
+                path.setLayoutY(setYPosition(state.getPosition())));
+
+        //Create the group to show when the aircraft is selected or when the zoom is very high
         createSelectedAircraft(state);
+
+        //Add the icon on the pane
         pane().getChildren().add(path);
     }
 
@@ -132,77 +135,102 @@ public final class AircraftController {
     }
 
     private void removeAircraft(ObservableAircraftState state) {
-        AircraftIcon icon = createIcon(state);
-        SVGPath path = new SVGPath();
-        path.setContent(icon.svgPath());
-        pane().getChildren().remove(path);
+        pane().getChildren().removeIf(node -> node.getId().equals(state.getIcaoAddress().string()));
     }
 
     private AircraftIcon createIcon(ObservableAircraftState state) {
         AircraftData data = state.getAircraftData();
-        AircraftIcon icon = data != null ?
-                AircraftIcon.iconFor(data.typeDesignator() != null ? data.typeDesignator() : new AircraftTypeDesignator(""),
-                        data.description() != null ? data.description() : new AircraftDescription(""),
-                        state.getCategory(), data.wakeTurbulenceCategory() != null ? data.wakeTurbulenceCategory() : WakeTurbulenceCategory.UNKNOWN)
+        AircraftIcon icon = data != null
+                ? AircraftIcon.iconFor(data.typeDesignator() != null ? data.typeDesignator() : new AircraftTypeDesignator(""),
+                data.description() != null ? data.description() : new AircraftDescription(""),
+                state.getCategory(),
+                data.wakeTurbulenceCategory() != null ? data.wakeTurbulenceCategory() : WakeTurbulenceCategory.UNKNOWN)
                 : AircraftIcon.iconFor(new AircraftTypeDesignator(""), new AircraftDescription(""), state.getCategory(),
                 WakeTurbulenceCategory.UNKNOWN);
         return icon;
     }
 
     private void createSelectedAircraft(ObservableAircraftState state) {
+        //Create all JFX Object
         Group icaoGroup = new Group();
         Group mid = new Group();
         Group label = new Group();
         Rectangle rectangle = new Rectangle();
         Text text = new Text();
         Group trajectoryGroup = new Group();
+
+        //Linking object together
         label.getChildren().addAll(rectangle, text);
         mid.getChildren().add(label);
         icaoGroup.getChildren().addAll(mid, trajectoryGroup);
         icaoGroup.setId("adr.\u2002OACI");
         label.getStyleClass().add("label");
-        state.airbornePosProperty().addListener((ListChangeListener<? super ObservableAircraftState.AirbornePos>) c -> {
-            if (trajectoryGroup.isVisible()) {
-                createTrajectoryGroup(trajectoryGroup, (List<ObservableAircraftState.AirbornePos>) c.getList());
-            }
-        });
         trajectoryGroup.getStyleClass().add("trajectory");
-        trajectoryGroup.visibleProperty().bind(Bindings.createBooleanBinding(() -> (selectedAircraft.get() == state), selectedAircraft));
-        trajectoryGroup.layoutXProperty().bind(Bindings.createDoubleBinding(() -> -parameters.getMinX() - icaoGroup.getLayoutX(),
-                parameters.minXProperty(), icaoGroup.layoutXProperty()));
-        trajectoryGroup.layoutYProperty().bind(Bindings.createDoubleBinding(() -> -parameters.getMinY() - icaoGroup.getLayoutY(),
-                parameters.minYProperty(), icaoGroup.layoutYProperty()));
+
+        //Set up listener/binding to the trajectory group
+        trajectoryGroup.visibleProperty().bind(
+                Bindings.createBooleanBinding(() -> (selectedAircraft.get() == state), selectedAircraft));
+        trajectoryGroup.layoutXProperty().bind(
+                Bindings.createDoubleBinding(() -> -parameters.getMinX() - icaoGroup.getLayoutX(),
+                        parameters.minXProperty(), icaoGroup.layoutXProperty()));
+        trajectoryGroup.layoutYProperty().bind(
+                Bindings.createDoubleBinding(() -> -parameters.getMinY() - icaoGroup.getLayoutY(),
+                        parameters.minYProperty(), icaoGroup.layoutYProperty()));
         trajectoryGroup.visibleProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue) {
                 createTrajectoryGroup(trajectoryGroup, state.getAirbornePos());
             }
         });
+
+        //Listener used to draw the trajectory if needed
+        state.airbornePosProperty().addListener((ListChangeListener<? super ObservableAircraftState.AirbornePos>) c -> {
+            if (trajectoryGroup.isVisible()) {
+                createTrajectoryGroup(trajectoryGroup, (List<ObservableAircraftState.AirbornePos>) c.getList());
+            }
+        });
+
+        //Listener used to draw the trajectory when changing the zoom value
         parameters.zoomProperty().addListener((observable, oldValue, newValue) -> {
             if (trajectoryGroup.isVisible()) {
                 createTrajectoryGroup(trajectoryGroup, state.getAirbornePos());
             }
         });
-        icaoGroup.layoutXProperty().bind(Bindings.createDoubleBinding(() ->
-                        WebMercator.x(parameters.getZoom(),
+
+        //Set up binding to update the position of the whole group according to the aircraft position
+        //and to know if it should be visible or not
+        icaoGroup.layoutXProperty().bind(
+                Bindings.createDoubleBinding(() -> WebMercator.x(parameters.getZoom(),
                                 state.getPosition().longitude()) - parameters.getMinX()
-                , parameters.zoomProperty(), state.positionProperty(), parameters.minXProperty()));
-        icaoGroup.layoutYProperty().bind(Bindings.createDoubleBinding(() ->
-                        WebMercator.y(parameters.getZoom(), state.getPosition().latitude()) - parameters.getMinY()
-                , parameters.zoomProperty(), state.positionProperty(), parameters.minYProperty()));
+                        , parameters.zoomProperty(), state.positionProperty(), parameters.minXProperty()));
+        icaoGroup.layoutYProperty().bind(
+                Bindings.createDoubleBinding(() -> WebMercator.y(parameters.getZoom(),
+                                state.getPosition().latitude()) - parameters.getMinY(),
+                        parameters.zoomProperty(), state.positionProperty(), parameters.minYProperty()));
+        icaoGroup.visibleProperty().bind(
+                Bindings.createBooleanBinding(() -> (parameters.getZoom() >= 11) || (selectedAircraft.get() == state),
+                        parameters.zoomProperty(), selectedAircraft));
+
+        //Bind the rectangle's dimension according to the text's dimension
         rectangle.widthProperty().bind(text.layoutBoundsProperty().map(bounds -> bounds.getWidth() + 4));
         rectangle.heightProperty().bind(text.layoutBoundsProperty().map(bounds -> bounds.getHeight() + 4));
-        text.setText(state.getIcaoAddress().string() + "\n" +
-                String.format("%f km/h", Units.convertFrom(state.getVelocity(), Units.Speed.KNOT)) + " " +
-                String.format("%f.0 m", state.getAltitude()));
-        text.textProperty().bind(Bindings.createStringBinding(() -> state.getIcaoAddress().string() + "\n" +
-                        String.format("%.0f km/h", Math.rint(Units.convertTo
-                                (state.getVelocity(), Units.Speed.KILOMETER_PER_HOUR))) + " " +
-                        String.format("%.0f m", Math.rint(state.getAltitude())), state.altitudeProperty(),
-                state.velocityProperty()));
-        icaoGroup.visibleProperty().bind(Bindings.createBooleanBinding(() -> (parameters.getZoom() >= 11) ||
-                (selectedAircraft.get() == state), parameters.zoomProperty(), selectedAircraft));
+
+        //Set and update the text with the correct values
+        text.setText(state.getIcaoAddress().string()
+                + "\n"
+                + (state.getVelocity() > 0d ? String.format("%f km/h", Units.convertFrom(state.getVelocity(), Units.Speed.KNOT)) : "?")
+                + " "
+                + (state.getAltitude() > 0 ? String.format("%f.0 m", state.getAltitude()) : "?"));
+        text.textProperty().bind(
+                Bindings.createStringBinding(() -> state.getIcaoAddress().string()
+                                + "\n"
+                                + String.format("%.0f km/h", Math.rint(Units.convertTo
+                                (state.getVelocity(), Units.Speed.KILOMETER_PER_HOUR)))
+                                + " "
+                                + String.format("%.0f m", Math.rint(state.getAltitude())), state.altitudeProperty(),
+                        state.velocityProperty()));
+
+        //Add the group to the pane
         pane().getChildren().add(icaoGroup);
-        rectangle.setOpacity(1d);
     }
 
     private void createTrajectoryGroup(Group trajectoryGroup, List<ObservableAircraftState.AirbornePos> list) {
@@ -220,8 +248,10 @@ public final class AircraftController {
                 if (Math.floor(last.altitude()) == Math.floor(current.altitude())) {
                     line.setStroke(ColorRamp.PLASMA.at(colorFormula(current.altitude())));
                 } else {
-                    LinearGradient linearGradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE,
-                            new Stop(0, ColorRamp.PLASMA.at(colorFormula(last.altitude()))), new Stop(1, ColorRamp.PLASMA.at(colorFormula(current.altitude()))));
+                    LinearGradient linearGradient = new LinearGradient(
+                            0, 0, 1, 0, true, CycleMethod.NO_CYCLE,
+                            new Stop(0, ColorRamp.PLASMA.at(colorFormula(last.altitude()))),
+                            new Stop(1, ColorRamp.PLASMA.at(colorFormula(current.altitude()))));
                     line.setStroke(linearGradient);
                 }
                 trajectoryGroup.getChildren().add(line);
